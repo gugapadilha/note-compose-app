9357de8314360f01566e1d2b958365491f0cd1a1
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-12-18 18:14:33 -0300

    search notes to android and ios done

--- shared/src/commonMain/kotlin/com/plcoding/noteappkmm/domain/data/note/SearchNotes.kt
+++ shared/src/commonMain/kotlin/com/plcoding/noteappkmm/domain/data/note/SearchNotes.kt
@@ -1,11 +1,15 @@
 package com.plcoding.noteappkmm.domain.data.note
 
 import com.plcoding.noteappkmm.domain.note.Note
+import com.plcoding.noteappkmm.domain.time.DateTimeUtil
 
 class SearchNotes {
     fun execute(note: List<Note>, query: String): List<Note> {
         if (query.isBlank()){
             return note
         }
+        return note.filter {
+            it.title.trim().lowercase().contains(query.lowercase()) || it.content.trim().lowercase().contains(query.lowercase())
+        }.sortedBy { DateTimeUtil.toEpochMillis(it.created) }
     }
 }
