0a33cb6fcb8aaf0c43d816627a81f24bddfaa609
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-12-19 16:45:20 -0300

    setting up view model to notes and search notes

---
+++ androidApp/src/main/java/com/plcoding/noteappkmm/android/node_list/NoteListViewModel.kt
@@ -0,0 +1,36 @@
+package com.plcoding.noteappkmm.android.node_list
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.noteappkmm.domain.data.note.SearchNotes
+import com.plcoding.noteappkmm.domain.note.Note
+import com.plcoding.noteappkmm.domain.note.NoteDataSource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteListViewModel @Inject constructor(
+    private var noteDataSource: NoteDataSource,
+    private val savedStateHandle: SavedStateHandle
+): ViewModel(){
+
+    private val searchNotes = SearchNotes()
+
+    private val notes = savedStateHandle.getStateFlow("notes", emptyList<Note>())
+    private val searchText = savedStateHandle.getStateFlow("searchText", "")
+    private val isSearchActive = savedStateHandle.getStateFlow("isSearchActive", false)
+
+    // If any of this parameters change, we will recalculate the note list state
+    val state = combine(notes, searchText, isSearchActive) {notes, searchText, isSearchActive ->
+        NoteListState(
+            //When notes or searchTexts change we will re-execute the searchNotes to reflex the latest state
+            notes = searchNotes.execute(notes, searchText),
+            searchText = searchText,
+            isSearchActive = isSearchActive
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), NoteListState())
+}
