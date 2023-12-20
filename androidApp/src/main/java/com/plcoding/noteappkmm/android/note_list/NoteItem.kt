f997b592cc60a4fac889fe3c4046dd75248f08b6
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-12-20 12:37:58 -0300

    starting creating the structure to individual note item

---
+++ androidApp/src/main/java/com/plcoding/noteappkmm/android/note_list/NoteItem.kt
@@ -0,0 +1,17 @@
+package com.plcoding.noteappkmm.android.note_list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.plcoding.noteappkmm.domain.note.Note
+
+@Composable
+fun NoteItem(
+    note: Note,
+    backgroundColor: Color,
+    onNoteClick: () -> Unit,
+    onDeleteClick: () -> Unit,
+    modifier: Modifier
+){
+
+}
