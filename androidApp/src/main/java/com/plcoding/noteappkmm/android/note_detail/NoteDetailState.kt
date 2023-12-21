4e29c766e44b71a0627219407beea5a5d56b77aa
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-12-21 14:03:08 -0300

    create note detail state to especifications note

---
+++ androidApp/src/main/java/com/plcoding/noteappkmm/android/note_detail/NoteDetailState.kt
@@ -0,0 +1,12 @@
+package com.plcoding.noteappkmm.android.note_detail
+
+import androidx.compose.ui.graphics.Color
+
+
+data class NoteDetailState(
+    val noteTitle: String = "",
+    val isNoteTitleTextFocused: Boolean = false,
+    val noteContent: String = "",
+    val isNoteContentTextFocused: Boolean  = false,
+    val noteColor: Color = Color.White
+)
